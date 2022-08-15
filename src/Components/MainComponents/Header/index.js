26c9a12847260dfb28434e226476df8576b854e6
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-08-15 13:36:49 -0300

    adquirindo informações de data
    criação da lógica responsavel pelas informações de data que serão mostradas no Header

--- src/Components/MainComponents/Header/index.js
+++ src/Components/MainComponents/Header/index.js
@@ -1,12 +1,39 @@
+//Packages
+import { useEffect, useState } from 'react';
+
 //Css
 import './Header.css';
 
 
 const Header = () => {
 
+    const [ dateData, setDateData ] = useState();
+
+    //Obter informações sobre data
+    useEffect(() => {
+
+        const monthsList = [ 'janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho',
+                             'agosto', 'setembro', 'outubro', 'novembro', 'dezembro' ];
+
+        const daysOfTheWeekList = [ 'domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 
+                                    'quinta-feira', 'sexta-feira', 'sábado' ];
+
+        const date = new Date();
+        const day = date.getDate();
+        const month = monthsList[date.getMonth()];
+        const year = date.getFullYear();
+        const dayOfTheWeek = daysOfTheWeekList[date.getDay()];
+
+        setDateData( { day, month, year, dayOfTheWeek } );
+
+    }, []);
+
+
     return(
         <header>
-            header
+            { dateData &&
+                <span>{dateData.day},{dateData.dayOfTheWeek} - {dateData.month},{dateData.year}</span>
+            }
         </header>
     );
 
